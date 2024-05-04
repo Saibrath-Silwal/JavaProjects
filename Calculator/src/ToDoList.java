b1c0b36d4e32f413be91cf476e2d7372d2a12d2d
Saibrath Silwal <157085001+Saibrath-Silwal@users.noreply.github.com>
2024-05-04 15:56:13 -0400

    Update ToDoList.java
    This program was not fully completed before. possible changes in future

--- Calculator/src/ToDoList.java
+++ Calculator/src/ToDoList.java
@@ -1,9 +1,38 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ToDoList{
     public static void main(String[] args){
+        ArrayList<String> ToDoList = new ArrayList<String>(); // Create an empty ArrayList
+
         Scanner scan = new Scanner(System.in);
-        String var1 = scan.nextLine();
+        int input;
+       do {
+           //making a menu
+           System.out.println("What would you like to do?");
+           System.out.println("1: Add a new task");
+           System.out.println("2: view all the task");
+           System.out.println("3: mark the task as completed");
+           System.out.println("4: delete the task");
+           System.out.println("5: exit");
+
+           input = scan.nextInt();
+           scan.nextLine();
+
+           switch(input){
+               case 1:
+                   System.out.println("Enter the task");
+                   String task = scan.nextLine();
+                   ToDoList.add(task);
+                   break;
+               case 2:
+                   System.out.println(ToDoList);
+                   break;
+               case 3:
+
+           }
+       }while(input != 5);
         scan.close();
     }
 }
