99b8c0d9e395620085c55a103e32d1e8a2445e82
Saibrath Silwal <157085001+Saibrath-Silwal@users.noreply.github.com>
2024-06-19 17:01:42 -0400

    Update Main.java
    while loop added to the calculator to make it run more than one

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -2,24 +2,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("enter the symbol");
-        char operator = scan.nextLine().charAt(0);
-        System.out.println("enter the first number");
-        double var1 = scan.nextDouble();
-        System.out.println("enter the second number");
-        double var2 = scan.nextDouble();
+        char x = 'x';
+        while (x=='x'){
+            System.out.println("enter the symbol");
+            char operator = scan.nextLine().charAt(0);
+            System.out.println("enter the first number");
+            double var1 = scan.nextDouble();
+            System.out.println("enter the second number");
+            double var2 = scan.nextDouble();
 
-        switch(operator){
-            case '+':
-                System.out.println(var1+var2);
-            case '-':
-                System.out.println(var1-var2);
-            case '*':
-                System.out.println(var1*var2);
-            case '/':
-                System.out.println(var1/var2);
+            switch(operator){
+                case '+':
+                    System.out.println(var1+var2);
+                    break;
+                case '-':
+                    System.out.println(var1-var2);
+                    break;
+                case '*':
+                    System.out.println(var1*var2);
+                    break;
+                case '/':
+                    System.out.println(var1/var2);
+                    break;
+
+
+            }
+            System.out.println("press any key to exit");
+            System.out.println("press x to continue");
+            x = scan.next().charAt(0);
+
         }
 
-          scan.close();
+        scan.close();
     }
 }
